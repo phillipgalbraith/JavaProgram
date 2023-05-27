c947ebb7d0d56019a55a51f2f53082eaaa203909
Phillip Galbraith <81811191+topstock@users.noreply.github.com>
2023-05-27 16:41:19 -0700

    calls to methods in the same class need static key word

--- src/QuickStart/QuickStart.java
+++ src/QuickStart/QuickStart.java
@@ -2,7 +2,11 @@ public class QuickStart{
 
     public static void main(String[] args){
         System.out.println(Dog.bark());
-
+        double[] bases = {1.2, 3.2, 5.4};
+        double[] heights = {8.7, 6.5, 4.3};
+        for (int i = 0; i < bases.length; i++) {
+            calcAreaTriangle(bases[i], heights[i]);
+        }
     }
 
     public class Dog{
@@ -11,5 +15,18 @@ public class QuickStart{
         public static String bark(){
             return "bark";
         }
+        
+    }
+
+    public static double calcAreaTriangle(double height, double base){
+        double area = height*base/2;
+        String areaMsg = "triangle with base "
+          .concat(String.valueOf(base))
+          .concat(" and height ")
+          .concat(String.valueOf(height))
+          .concat(" has area ")
+          .concat(String.valueOf(area));
+        System.out.println(areaMsg);
+        return area;
     }
 }
