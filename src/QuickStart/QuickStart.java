c8d8518e70590854710cd0ee2113d5bf05f55cb2
Phillip Galbraith <81811191+topstock@users.noreply.github.com>
2023-05-27 16:22:41 -0700

    added class with state and behavior

--- src/QuickStart/QuickStart.java
+++ src/QuickStart/QuickStart.java
@@ -1,6 +1,15 @@
 public class QuickStart{
 
     public static void main(String[] args){
-        System.out.println("Hello, world!"); 
+        System.out.println(Dog.bark());
+
+    }
+
+    public class Dog{
+        String breed = "shi-tzu";
+
+        public static String bark(){
+            return "bark";
+        }
     }
 }
